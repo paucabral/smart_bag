de37a3569427d71c7c2c2621aafe4d76936e14eb
Jose Paulo Cabral <josepaucabral@gmail.com>
2019-12-28 19:33:26 +0800

    modified allowable hosts for multiple device hosting

--- smart_bag/smart_bag/settings.py
+++ smart_bag/smart_bag/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = ')0d*0ki&rl3x^c^ekt*3tnl0x00vs6iyz6*+(+7gfblluhew_n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
